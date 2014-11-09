e9d0a6e8db0ed8f16d632302fc069a84a456c70e
Lupita <carabeslupita@gmail.com>
2014-11-09 15:35:19 -0800

    Carabes Frames
    11/8/14

---
+++ LincBand/app/src/main/java/com/lincbandapp/lincband/MainActivity.java
@@ -0,0 +1,45 @@
+package com.lincbandapp.lincband;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.ImageButton;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // initialize buttons
+        ImageButton viewChainButton = (ImageButton) findViewById(R.id.viewChainButton);
+        ImageButton bluetoothButton = (ImageButton) findViewById(R.id.bluetoothButton);
+        ImageButton tutorialButton = (ImageButton) findViewById(R.id.tutorialButton);
+
+        //set up button listeners
+        viewChainButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent viewChain = new Intent(getApplicationContext(),ChainListActivity.class);
+                startActivity(viewChain);
+            }
+        });
+        bluetoothButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent bluetooth = new Intent(getApplicationContext(),EnableBTActivity.class);
+                startActivity(bluetooth);
+            }
+        });
+        tutorialButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent tutorial = new Intent(getApplicationContext(),Tutorial_1_Activity.class);
+                startActivity(tutorial);
+            }
+        });
+    }
+}
---
+++ LincBand/app/src/main/java/com/lincbandapp/lincband/SplashScreenActivity.java
@@ -0,0 +1,36 @@
+package com.lincbandapp.lincband;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreenActivity extends Activity {
+
+    // Splash Screen duration
+    private static int SPLASH_TIME_OUT = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+
+			/*
+			 * Showing splash screen of the Linc app logo
+			 */
+
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                // Start your app main activity
+                Intent i = new Intent(SplashScreenActivity.this, MainActivity.class);
+                startActivity(i);
+
+                // close this activity
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+}
